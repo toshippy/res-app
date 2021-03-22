e8d136bf9a7dbd41a26fe35d0a023f70644fb03d
toshippy <toshippy@gmail.com>
2021-03-22 15:09:04 +0900

    front作成

---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　',
+        headingtext2: 'abcdefgahskdvoenfvn;wwvddvv',
+        headingtext3: 'サンプル文章　サンプル文章　サンプル文章　サンプル文章　'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A large phone with one of the best camera',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　',
+        headingtext2: 'abcdefgahskdvoenfvn;wwvddvv',
+        headingtext3: 'サンプル文章　サンプル文章　サンプル文章　サンプル文章　'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　',
+        headingtext2: 'abcdefgahskdvoenfvn;wwvddvv',
+        headingtext3: 'サンプル文章　サンプル文章　サンプル文章　サンプル文章　'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: '',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　',
+        headingtext2: 'abcdefgahskdvoenfvn;wwvddvv',
+        headingtext3: 'サンプル文章　サンプル文章　サンプル文章　サンプル文章　'
+    }
+];
